b30b9f6a351f296cf58040a32ea6ae55322888ff
amandamich <amandamdarwis@gmail.com>
2025-10-27 23:13:29 +0700

    Initial commit

---
+++ DriveBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DriveBuddy
+//
+//  Created by Timothy on 27/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, Welcome to Drive Buddy!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DriveBuddy/DriveBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveBuddyApp.swift
+//  DriveBuddy
+//
+//  Created by Timothy on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DriveBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
